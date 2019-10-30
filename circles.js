da09d336fca10220b6d6beaa2770df453a9c635a
Mena Wang <wang.mena@gmail.com>
2019-10-30 01:00:09 -0400

    Make circles disappear after 20 circles are on the screen

--- circles.js
+++ circles.js
@@ -1,9 +1,26 @@
 /////////////   MAIN FUNCTIONS  ////////////////
 
+var listOfCircles;
+
 function setup() {
 	var c = document.getElementById("mainCanvas"); // gets canvas element from HTML
 	c.width = window.innerWidth;     // give canvas same width...
 	c.height = window.innerHeight;   // and height as our window.
+	listOfCircles = [];
+}
+
+// list would include [x, y, rad]
+function deleteCircle(){
+	var c = document.getElementById("mainCanvas");
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+
+	var circle = listOfCircles.shift();
+	ctx.strokeStyle = "rgb(255, 255, 255)"; // replace with background color
+	ctx.lineWidth = 10; 
+
+	ctx.arc(circle[0], circle[1], circle[2], 0, 2 * Math.PI);
+	ctx.stroke();
 }
 
 function reactKey(evt) {
@@ -15,6 +32,8 @@ function reactKey(evt) {
 	var centerY = Math.random() * c.height; // returns something in [0, 1] * c.height
 	var rad = Math.random() * 50 + 40;
 
+	listOfCircles.push([centerX, centerY, rad]);
+
 	// Extra function to randomly generate color values (0-255)
 	var r = function() { return Math.random() * 256 };
 
@@ -24,6 +43,10 @@ function reactKey(evt) {
 
 	ctx.arc(centerX, centerY, rad, 0, 2 * Math.PI);
 	ctx.stroke();
+
+	if (listOfCircles.length === 20) {
+		deleteCircle();
+	}
 }
 
 /////////////   SETUP THE HANDLERS  ////////////////
