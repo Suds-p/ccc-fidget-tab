3e248608baf4aa8cea718733799ff2b31b588c9c
Mena Wang <wang.mena@gmail.com>
2019-10-30 01:19:11 -0400

    clearing canvas and redrawing instead of drawing white circles

--- circles.js
+++ circles.js
@@ -10,42 +10,46 @@ function setup() {
 }
 
 // list would include [x, y, rad]
-function deleteCircle(){
-	var c = document.getElementById("mainCanvas");
-	var ctx = c.getContext("2d");
-	ctx.beginPath();
+// function deleteCircle(){
+// 	var c = document.getElementById("mainCanvas");
+// 	var ctx = c.getContext("2d");
+// 	ctx.beginPath();
 
-	var circle = listOfCircles.shift();
-	ctx.strokeStyle = "rgb(255, 255, 255)"; // replace with background color
-	ctx.lineWidth = 10; 
+// 	var circle = listOfCircles.shift();
+// 	ctx.strokeStyle = "rgb(255, 255, 255)"; // replace with background color
+// 	ctx.lineWidth = 10; 
 
-	ctx.arc(circle[0], circle[1], circle[2], 0, 2 * Math.PI);
-	ctx.stroke();
-}
+// 	ctx.arc(circle[0], circle[1], circle[2], 0, 2 * Math.PI);
+// 	ctx.stroke();
+// }
 
 function reactKey(evt) {
 	var c = document.getElementById("mainCanvas");
 	var ctx = c.getContext("2d");
-	ctx.beginPath();
-
-	var centerX = Math.random() * c.width;  // returns something in [0, 1] * c.width
-	var centerY = Math.random() * c.height; // returns something in [0, 1] * c.height
-	var rad = Math.random() * 50 + 40;
-
-	listOfCircles.push([centerX, centerY, rad]);
+	ctx.clearRect(0, 0, c.width, c.height);
 
 	// Extra function to randomly generate color values (0-255)
 	var r = function() { return Math.random() * 256 };
 
-	// Call r from above 3 times to make random numbers in string
-	ctx.strokeStyle = "rgb(" + r() + ", " + r() + ", " + r() + ")";
-	ctx.lineWidth = 8;
+	var centerX = Math.random() * c.width;  // returns something in [0, 1] * c.width
+	var centerY = Math.random() * c.height; // returns something in [0, 1] * c.height
+	var rad = Math.random() * 50 + 40;
+	var color = "rgb(" + r() + ", " + r() + ", " + r() + ")";
+	var lineWidth = 8;
 
-	ctx.arc(centerX, centerY, rad, 0, 2 * Math.PI);
-	ctx.stroke();
+	listOfCircles.push([centerX, centerY, rad, color, lineWidth]);
 
-	if (listOfCircles.length === 20) {
-		deleteCircle();
+	if (listOfCircles.length === 5) {
+		listOfCircles.shift();
+	}
+
+	for (var i = 0; i < listOfCircles.length; i++){
+		ctx.beginPath();
+		ctx.strokeStyle = listOfCircles[i][3]; 
+		ctx.lineWidth = listOfCircles[i][4];
+
+		ctx.arc(listOfCircles[i][0], listOfCircles[i][1], listOfCircles[i][2], 0, 2 * Math.PI);
+		ctx.stroke();
 	}
 }
 
