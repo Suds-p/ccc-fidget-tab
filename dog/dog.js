cbfcc6d448370712467544d4622904b3509619c0
Jennifer Lee <jjl296@cornell.edu>
2019-11-02 17:11:44 -0400

    attempt to calculate angle

--- dog/dog.js
+++ dog/dog.js
@@ -6,8 +6,13 @@ function reactMove(evt) {
   var coor = "X coords: " + x + ", Y coords: " + y;
   console.log(coor);
 
-  //do some math
-  var degrees = 0;
+  //get coordinates of tongue area of specific dog 
+  var dogX = 532;
+  var dogY = 419;
+
+  //calculate angle between mouse coordinate and tongue coordinate
+  var degrees = (180 / Math.PI) * Math.atan((x - dogX) / y - dogY);
+  console.log(degrees);
 
   document.getElementById("tongue").style.transform = "rotate(".concat(degrees.toString(), "deg)");
 
