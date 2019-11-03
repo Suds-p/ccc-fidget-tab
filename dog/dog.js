58746af1a1156d2ac79525b9de61f1647cb43a4a
Mena Wang <wang.mena@gmail.com>
2019-11-02 21:47:34 -0400

    Clicking will make dog image appear

--- dog/dog.js
+++ dog/dog.js
@@ -1,5 +1,34 @@
 /////////////   MAIN FUNCTIONS  ////////////////
 
+//make another dog appear on click
+function click(evt) {
+  var div = document.createElement("div");
+  
+  //dog image
+  var dog = document.createElement("img");
+  dog.src = "dog.png";
+
+  var original_width = dog.width; 
+  var original_height = dog.height; 
+
+  dog.width = Math.random() * 200 + 50;
+  dog.height = dog.width * (original_width / original_height);
+
+  div.style.position = "fixed";
+  div.style.marginLeft = evt.clientX - (dog.width / 2) + 'px'; 
+  div.style.marginTop = evt.clientY - (dog.height / 2) + 'px';
+
+  // tongue.src="tongue.png";
+  // tongue.class="tongue";
+
+  div.appendChild(dog);
+
+  var tongue = document.createElement("img");
+  div.appendChild(tongue);
+
+  document.body.appendChild(div); 
+}
+
 function reactMove(evt) {
   var x = evt.clientX;     // Get the horizontal coordinate
   var y = evt.clientY;
@@ -21,3 +50,4 @@ function reactMove(evt) {
 /////////////   SETUP THE HANDLERS  ////////////////
 
 document.addEventListener("mousemove", reactMove);
+document.addEventListener("mousedown", click);
