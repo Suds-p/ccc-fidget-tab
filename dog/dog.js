bd9d98b156fcbf29ab9982df16ae916b5dc4a46f
Jennifer Lee <jjl296@cornell.edu>
2019-11-09 14:43:55 -0500

    successful tongue rotation

--- dog/dog.js
+++ dog/dog.js
@@ -3,19 +3,19 @@
 //make another dog appear on click
 function click(evt) {
   var div = document.createElement("div");
-  
+
   //dog image
   var dog = document.createElement("img");
   dog.src = "dog.png";
 
-  var original_width = dog.width; 
-  var original_height = dog.height; 
+  var original_width = dog.width;
+  var original_height = dog.height;
 
   dog.width = Math.random() * 200 + 50;
   dog.height = dog.width * (original_width / original_height);
 
   div.style.position = "fixed";
-  div.style.marginLeft = evt.clientX - (dog.width / 2) + 'px'; 
+  div.style.marginLeft = evt.clientX - (dog.width / 2) + 'px';
   div.style.marginTop = evt.clientY - (dog.height / 2) + 'px';
 
   // tongue.src="tongue.png";
@@ -26,7 +26,7 @@ function click(evt) {
   var tongue = document.createElement("img");
   div.appendChild(tongue);
 
-  document.body.appendChild(div); 
+  document.body.appendChild(div);
 }
 
 function reactMove(evt) {
@@ -40,8 +40,10 @@ function reactMove(evt) {
   var dogY = 419;
 
   //calculate angle between mouse coordinate and tongue coordinate
-  var degrees = (180 / Math.PI) * Math.atan((x - dogX) / y - dogY);
-  console.log(degrees);
+  var degrees = (180 / Math.PI) * Math.atan((dogX - x) / (y - dogY)) + 45;
+  console.log(degrees - 45);
+
+  if (y < dogY) { degrees += 180; }
 
   document.getElementById("tongue").style.transform = "rotate(".concat(degrees.toString(), "deg)");
 
