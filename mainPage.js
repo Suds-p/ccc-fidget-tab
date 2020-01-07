e1b1dba4fa64e0e8315f22fcb0dab8f00f89c567
Sudeshna <slp256@cornell.edu>
2020-01-07 02:26:08 -0500

    Add prototype list to select fidgets and rename files for easier automatic selection

---
+++ mainPage.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.trigger, .popup').click(function() {
+		$('.popup').toggleClass('close');
+	});
+});
+
+function runWidget() {
+	var opts = document.getElementById('widget-select').options;
+	var selected = opts[opts.selectedIndex].value;
+	if (['dog', 'Greetings', 'KoiPond', 'Rainbow'].includes(selected)) {
+		window.location.href = `/${selected}/main.html`;
+	}
+}
+
+document.getElementById('widget-select-btn').addEventListener('click', runWidget);
